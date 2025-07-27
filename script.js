b13e2c8e89c6afa8ec8dbe112cdcee08e678cc57
Khushisoni830 <khu830shi@gmail.com>
2025-07-27 23:57:18 +0530

    Initial code files added

---
+++ script.js
@@ -0,0 +1,18 @@
+// Optional - Add smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+const toggleMode = document.getElementById('toggle-mode');
+
+toggleMode.addEventListener('change', () => {
+  document.body.classList.toggle('light');
+});
+// Re-init AOS after content update or theme toggle
+setTimeout(() => {
+  AOS.refresh();
+}, 100);
